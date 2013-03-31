ba81650067c1216c8013fa8bfef08cf3dabccdb1
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2013-03-31 01:42:17 +0100

    CMake now builds the system module

--- cmake/Android.mk
+++ cmake/Android.mk
@@ -1,1 +1,8 @@
+LOCAL_PATH := $(call my-dir)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := sfml-system
+LOCAL_SRC_FILES := lib/$(TARGET_ARCH_ABI)/libsfml-system.a
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
+
+include $(PREBUILT_STATIC_LIBRARY)
