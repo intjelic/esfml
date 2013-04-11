0ccd8f4aab2334e1b42f5caac93856ff7334fa7f
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2013-04-11 08:33:43 +0200

    Added jpeg and freetype library to extlibs for Android

--- extlibs/android/extlibs/Android.mk
+++ extlibs/android/extlibs/Android.mk
@@ -1,1 +1,13 @@
+LOCAL_PATH := $(call my-dir)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := freetype
+LOCAL_SRC_FILES := lib/$(TARGET_ARCH_ABI)/libfreetype.a
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := jpeg
+LOCAL_SRC_FILES := lib/$(TARGET_ARCH_ABI)/libjpeg.a
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
+include $(PREBUILT_STATIC_LIBRARY)
