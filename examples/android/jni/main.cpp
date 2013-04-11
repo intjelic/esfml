9a051c78850d7212c9faa5a3e88686e0c9461b58
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2013-04-11 07:11:13 +0200

    Updated android example

--- examples/android/jni/main.cpp
+++ examples/android/jni/main.cpp
@@ -1,6 +1,24 @@
 #include <sfml/system.hpp>
+#include <sfml/window.hpp>
+#include <GLES/gl.h>
 
 int main(int argc, char *argv[])
 {
-	sf::sleep(sf::seconds(2));
+    sf::Window window(sf::VideoMode::getDesktopMode(), "");
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        if (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+
+        glClearColor(0.5, 0.5, 0.5, 1);
+        glClear(GL_COLOR_BUFFER_BIT);
+        window.display();
+    }
 }
