9c83e6d8a4aea51f69bbd4fa0aa91e35501ee40d
Michael Bradshaw <mjbshaw@gmail.com>
2013-05-23 19:58:14 -0600

    Update the example to deal with size changes

--- examples/android/jni/main.cpp
+++ examples/android/jni/main.cpp
@@ -11,21 +11,23 @@ int main(int argc, char *argv[])
 
     sf::Texture texture;
     if(!texture.loadFromFile("image.png"))
-    	return EXIT_FAILURE;
+        return EXIT_FAILURE;
 
     sf::Sprite sprite(texture);
 
     sf::Vector2u imageSize = texture.getSize();
     sprite.setOrigin(imageSize.x/2, imageSize.y/2);
 
-    sf::Vector2u screenSize = window.getSize();
-    sprite.setPosition(screenSize.x/2, screenSize.y/2);
-
     sf::Music music;
     if(!music.openFromFile("orchestral.ogg"))
-    	return EXIT_FAILURE;
+        return EXIT_FAILURE;
 
     music.play();
+    
+    // Set the world's center to the center of the screen
+    sf::View view = window.getView();
+    view.setCenter(0, 0);
+    window.setView(view);
 
     while (window.isOpen())
     {
@@ -38,7 +40,17 @@ int main(int argc, char *argv[])
             }
             else if (event.type == sf::Event::MouseMoved)
             {
-                sprite.setPosition(event.mouseMove.x, event.mouseMove.y);
+                // Move the sprite to the finger's location
+                sf::Vector2i pixelPos(event.mouseMove.x, event.mouseMove.y);
+                sf::Vector2f worldPos = window.mapPixelToCoords(pixelPos);
+                sprite.setPosition(worldPos);
+            }
+            else if (event.type == sf::Event::Resized)
+            {
+                // Resize the view so it's not stretched
+                sf::View view = window.getView();
+                view.setSize((float)event.size.width, (float)event.size.height);
+                window.setView(view);
             }
         }
 
