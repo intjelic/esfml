06f7a85a2e3a1fd10117f9f0389db9dc182f2451
Michael Bradshaw <mjbshaw@gmail.com>
2013-05-27 11:47:37 -0600

    Make quads available for rendering with
    While we won't use them internally, custom shapes from users may
    be drawn with quads.

--- include/sfml/graphics/PrimitiveType.hpp
+++ include/sfml/graphics/PrimitiveType.hpp
@@ -44,7 +44,8 @@ enum PrimitiveType
     LinesStrip,     ///< List of connected lines, a point uses the previous point to form a line
     Triangles,      ///< List of individual triangles
     TrianglesStrip, ///< List of connected triangles, a point uses the two previous points to form a triangle
-    TrianglesFan    ///< List of connected triangles, a point uses the common center and the previous point to form a triangle
+    TrianglesFan,   ///< List of connected triangles, a point uses the common center and the previous point to form a triangle
+    Quads           ///< List of individual quads
 };
 
 } // namespace sf
