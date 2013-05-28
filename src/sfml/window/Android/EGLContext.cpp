1a7b110e38897c3ad4280b98149ce57eceb75a86
Michael Bradshaw <mjbshaw@gmail.com>
2013-05-28 10:38:44 -0600

    Properly terminate the attribute list

--- src/sfml/window/Android/EGLContext.cpp
+++ src/sfml/window/Android/EGLContext.cpp
@@ -68,7 +68,7 @@ m_surface (EGL_NO_SURFACE)
     eglCheck(eglChooseConfig(m_display, attribs, &config[0], 1, &numConfigs));
 
     // Note: removing attrib_list crashes the app... idk why
-    EGLint attrib_list[] = { EGL_TEXTURE_TARGET };
+    EGLint attrib_list[] = { EGL_TEXTURE_TARGET, EGL_NONE };
     m_surface = eglCheck(eglCreatePbufferSurface(m_display, config[0], attrib_list));
 
     // Create the context
