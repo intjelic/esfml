44b8e38a08098f8ffa5ee3008e6ee5507d3eff09
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2013-05-31 04:29:23 +0200

    Added user_interface example

---
+++ examples/user_interface/user_interface.cpp
@@ -0,0 +1,13 @@
+#include <sfml/system.hpp>
+#include <sfml/window.hpp>
+#include <sfml/graphics.hpp>
+#include <sfml/gui.hpp>
+
+int main()
+{
+    sf::GuiWindow window;
+
+    window.main();
+
+    return EXIT_SUCCESS;
+}
