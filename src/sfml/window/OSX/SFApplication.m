aa6e046f657c4bc2d47ec4ffa206ed566158eea2
Marco Antognini <antognini.marco@gmail.com>
2013-06-27 15:22:56 +0200

    Fix KeyRelease event with CMD key pressed (close #381)
    Solution based on pull request #401
    
    Conflicts:
    	src/sfml/window/OSX/SFApplication.m
    	src/sfml/window/OSX/WindowImplCocoa.mm

--- src/sfml/window/OSX/SFApplication.m
+++ src/sfml/window/OSX/SFApplication.m
@@ -1,8 +1,8 @@
 ////////////////////////////////////////////////////////////
 //
 // SFML - Simple and Fast Multimedia Library
-// Copyright (C) 2007-2012 Marco Antognini (antognini.marco@gmail.com), 
-//                         Laurent Gomila (laurent.gom@gmail.com), 
+// Copyright (C) 2007-2012 Marco Antognini (antognini.marco@gmail.com),
+//                         Laurent Gomila (laurent.gom@gmail.com),
 //
 // This software is provided 'as-is', without any express or implied warranty.
 // In no event will the authors be held liable for any damages arising from the use of this software.
@@ -27,7 +27,6 @@
 // Headers
 ////////////////////////////////////////////////////////////
 #import <sfml/window/OSX/SFApplication.h>
-#import <AppKit/AppKit.h>
 
 
 ////////////////////////////////////////////////////////////
@@ -37,15 +36,25 @@
 ////////////////////////////////////////////////////////////
 +(void)processEvent
 {
-    [NSApplication sharedApplication]; // Make sure NSApp exists
+    [SFApplication sharedApplication]; // Make sure NSApp exists
     NSEvent* event = nil;
-    
+
     while ((event = [NSApp nextEventMatchingMask:NSAnyEventMask
                                        untilDate:[NSDate distantPast]
                                           inMode:NSDefaultRunLoopMode
                                          dequeue:YES])) // Remove the event from the dequeue
     {
         [NSApp sendEvent:event];
+    }
+}
+
+- (void)sendEvent:(NSEvent *)anEvent
+{
+    if ([anEvent type] == NSKeyUp) {
+        [[[self mainWindow] firstResponder] tryToPerform:@selector(keyUp:)
+                                                    with:anEvent];
+    } else {
+        [super sendEvent:anEvent];
     }
 }
 
--- src/sfml/window/OSX/WindowImplCocoa.mm
+++ src/sfml/window/OSX/WindowImplCocoa.mm
@@ -1,8 +1,8 @@
 ////////////////////////////////////////////////////////////
 //
 // SFML - Simple and Fast Multimedia Library
-// Copyright (C) 2007-2012 Marco Antognini (antognini.marco@gmail.com), 
-//                         Laurent Gomila (laurent.gom@gmail.com), 
+// Copyright (C) 2007-2012 Marco Antognini (antognini.marco@gmail.com),
+//                         Laurent Gomila (laurent.gom@gmail.com),
 //
 // This software is provided 'as-is', without any express or implied warranty.
 // In no event will the authors be held liable for any damages arising from the use of this software.
@@ -34,6 +34,7 @@
 #import <sfml/window/OSX/SFViewController.h>
 #import <sfml/window/OSX/cpp_objc_conversion.h>
 #import <sfml/window/OSX/AutoreleasePoolWrapper.h>
+#import <sfml/window/OSX/SFApplication.h>
 
 namespace sf
 {
@@ -49,69 +50,69 @@ WindowImplCocoa::WindowImplCocoa(WindowHandle handle)
 {
     // Ask for a pool.
     retainPool();
-    
+
     // Treat the handle as it real type
     id nsHandle = (id)handle;
     if ([nsHandle isKindOfClass:[NSWindow class]]) {
-        
+
         // We have a window.
         m_delegate = [[SFWindowController alloc] initWithWindow:nsHandle];
-    
+
     } else if ([nsHandle isKindOfClass:[NSView class]]) {
-        
+
         // We have a view.
         m_delegate = [[SFViewController alloc] initWithView:nsHandle];
-        
+
     } else {
-        
+
         sf::err()
             << "Cannot import this Window Handle because it is neither "
             << "a <NSWindow*> nor <NSView*> object "
-            << "(or any of their subclasses). You gave a <" 
+            << "(or any of their subclasses). You gave a <"
             << [[nsHandle className] UTF8String]
             << "> object."
             << std::endl;
-        return;            
-        
+        return;
+
     }
-     
+
     [m_delegate setRequesterTo:this];
 }
-    
-    
+
+
 ////////////////////////////////////////////////////////////
-WindowImplCocoa::WindowImplCocoa(VideoMode mode, 
-                                 const String& title, 
+WindowImplCocoa::WindowImplCocoa(VideoMode mode,
+                                 const String& title,
                                  unsigned long style)
 : m_showCursor(true)
 {
     // Transform the app process.
     setUpProcess();
-    
+
     // Ask for a pool.
     retainPool();
-    
+
     m_delegate = [[SFWindowController alloc] initWithMode:mode andStyle:style];
     [m_delegate changeTitle:sfStringToNSString(title)];
     [m_delegate setRequesterTo:this];
 }
 
-    
+
 ////////////////////////////////////////////////////////////
 WindowImplCocoa::~WindowImplCocoa()
 {
     [m_delegate closeWindow];
-    
+
     [m_delegate release];
-    
+
     releasePool();
-    
+
     drainPool(); // Make sure everything was freed
     // This solve some issue when sf::Window::Create is called for the
     // second time (nothing was render until the function was called again)
 }
-    
-    
+
+
 ////////////////////////////////////////////////////////////
 void WindowImplCocoa::applyContext(NSOpenGLContextRef context) const
 {
@@ -123,20 +124,20 @@ void WindowImplCocoa::applyContext(NSOpenGLContextRef context) const
 void WindowImplCocoa::setUpProcess(void)
 {
     static bool isTheProcessSetAsApplication = false;
-    
+
     if (!isTheProcessSetAsApplication) {
         // Do it only once !
         isTheProcessSetAsApplication = true;
-        
+
         // Set the process as a normal application so it can get focus.
         ProcessSerialNumber psn;
         if (!GetCurrentProcess(&psn)) {
             TransformProcessType(&psn, kProcessTransformToForegroundApplication);
             SetFrontProcess(&psn);
         }
-        
+
         // Tell the application to stop bouncing in the Dock.
-        [[NSApplication sharedApplication] finishLaunching];
+        [[SFApplication sharedApplication] finishLaunching];
         // NOTE : This last call won't harm anything even if SFML window was
         // created with an external handle.
     }
@@ -145,18 +146,18 @@ void WindowImplCocoa::setUpProcess(void)
 
 #pragma mark
 #pragma mark WindowImplCocoa's window-event methods
-    
-    
+
+
 ////////////////////////////////////////////////////////////
 void WindowImplCocoa::windowClosed(void)
-{            
+{
     Event event;
     event.type = Event::Closed;
-    
+
     pushEvent(event);
 }
-    
-    
+
+
 ////////////////////////////////////////////////////////////
 void WindowImplCocoa::windowResized(unsigned int width, unsigned int height)
 {
@@ -164,42 +165,42 @@ void WindowImplCocoa::windowResized(unsigned int width, unsigned int height)
     event.type = Event::Resized;
     event.size.width  = width;
     event.size.height = height;
-    
+
     pushEvent(event);
 }
 
-    
+
 ////////////////////////////////////////////////////////////
 void WindowImplCocoa::windowLostFocus(void)
 {
     if (!m_showCursor) {
         [m_delegate showMouseCursor]; // Make sur the cursor is visible
     }
-    
+
     Event event;
     event.type = Event::LostFocus;
-    
+
     pushEvent(event);
 }
 
-    
+
 ////////////////////////////////////////////////////////////
 void WindowImplCocoa::windowGainedFocus(void)
 {
     if (!m_showCursor) {
         [m_delegate hideMouseCursor]; // Restore user's setting
     }
-    
+
     Event event;
     event.type = Event::GainedFocus;
-    
+
     pushEvent(event);
 }
-    
+
 #pragma mark
 #pragma mark WindowImplCocoa's mouse-event methods
-    
-    
+
+
 ////////////////////////////////////////////////////////////
 void WindowImplCocoa::mouseDownAt(Mouse::Button button, int x, int y)
 {
@@ -208,11 +209,11 @@ void WindowImplCocoa::mouseDownAt(Mouse::Button button, int x, int y)
     event.mouseButton.button = button;
     event.mouseButton.x = x;
     event.mouseButton.y = y;
-    
+
     pushEvent(event);
 }
 
-    
+
 ////////////////////////////////////////////////////////////
 void WindowImplCocoa::mouseUpAt(Mouse::Button button, int x, int y)
 {
@@ -221,7 +222,7 @@ void WindowImplCocoa::mouseUpAt(Mouse::Button button, int x, int y)
     event.mouseButton.button = button;
     event.mouseButton.x = x;
     event.mouseButton.y = y;
-    
+
     pushEvent(event);
 }
 
@@ -233,7 +234,7 @@ void WindowImplCocoa::mouseMovedAt(int x, int y)
     event.type = Event::MouseMoved;
     event.mouseMove.x = x;
     event.mouseMove.y = y;
-    
+
     pushEvent(event);
 }
 
@@ -245,7 +246,7 @@ void WindowImplCocoa::mouseWheelScrolledAt(float delta, int x, int y)
     event.mouseWheel.delta = delta;
     event.mouseWheel.x = x;
     event.mouseWheel.y = y;
-    
+
     pushEvent(event);
 }
 
@@ -255,49 +256,49 @@ void WindowImplCocoa::mouseMovedIn(void)
     if (!m_showCursor) {
         [m_delegate hideMouseCursor]; // Restore user's setting
     }
-    
+
     Event event;
     event.type = Event::MouseEntered;
-    
+
     pushEvent(event);
 }
-    
+
 ////////////////////////////////////////////////////////////
 void WindowImplCocoa::mouseMovedOut(void)
 {
     if (!m_showCursor) {
         [m_delegate showMouseCursor]; // Make sur the cursor is visible
     }
-    
+
     Event event;
     event.type = Event::MouseLeft;
-    
+
     pushEvent(event);
 }
-    
-    
+
+
 #pragma mark
 #pragma mark WindowImplCocoa's key-event methods
-    
-    
+
+
 ////////////////////////////////////////////////////////////
 void WindowImplCocoa::keyDown(Event::KeyEvent key)
 {
     Event event;
     event.type = Event::KeyPressed;
     event.key = key;
-    
+
     pushEvent(event);
 }
 
-    
+
 ////////////////////////////////////////////////////////////
 void WindowImplCocoa::keyUp(Event::KeyEvent key)
 {
     Event event;
     event.type = Event::KeyReleased;
     event.key = key;
-    
+
     pushEvent(event);
 }
 
@@ -308,11 +309,11 @@ void WindowImplCocoa::textEntered(unichar charcode)
     Event event;
     event.type = Event::TextEntered;
     event.text.unicode = charcode;
-    
+
     pushEvent(event);
 }
 
-    
+
 #pragma mark
 #pragma mark WindowImplCocoa's event-related methods
 
@@ -321,7 +322,7 @@ void WindowImplCocoa::processEvents()
 {
     [m_delegate processEvent];
 }
-    
+
 #pragma mark
 #pragma mark WindowImplCocoa's private methods
 
@@ -331,7 +332,7 @@ WindowHandle WindowImplCocoa::getSystemHandle() const
     return [m_delegate getSystemHandle];
 }
 
-    
+
 ////////////////////////////////////////////////////////////
 Vector2i WindowImplCocoa::getPosition() const
 {
@@ -354,28 +355,28 @@ Vector2u WindowImplCocoa::getSize() const
     return Vector2u(size.width, size.height);
 }
 
-    
+
 ////////////////////////////////////////////////////////////
 void WindowImplCocoa::setSize(const Vector2u& size)
-{    
+{
     [m_delegate resizeTo:size.x by:size.y];
 }
 
-    
+
 ////////////////////////////////////////////////////////////
 void WindowImplCocoa::setTitle(const String& title)
 {
     [m_delegate changeTitle:sfStringToNSString(title)];
 }
 
-    
+
 ////////////////////////////////////////////////////////////
 void WindowImplCocoa::setIcon(unsigned int width, unsigned int height, const Uint8* pixels)
 {
     [m_delegate setIconTo:width by:height with:pixels];
 }
 
-    
+
 ////////////////////////////////////////////////////////////
 void WindowImplCocoa::setVisible(bool visible)
 {
@@ -386,12 +387,12 @@ void WindowImplCocoa::setVisible(bool visible)
     }
 }
 
-    
+
 ////////////////////////////////////////////////////////////
 void WindowImplCocoa::setMouseCursorVisible(bool visible)
 {
     m_showCursor = visible;
-    
+
     if (m_showCursor) {
         [m_delegate showMouseCursor];
     } else {
@@ -399,7 +400,7 @@ void WindowImplCocoa::setMouseCursorVisible(bool visible)
     }
 }
 
-    
+
 ////////////////////////////////////////////////////////////
 void WindowImplCocoa::setKeyRepeatEnabled(bool enabled)
 {
@@ -410,7 +411,7 @@ void WindowImplCocoa::setKeyRepeatEnabled(bool enabled)
     }
 }
 
-    
+
 } // namespace priv
-    
+
 } // namespace sf
