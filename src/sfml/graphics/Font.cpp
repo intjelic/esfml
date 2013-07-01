d1d94b3c24e429b28ba38bf2379ba43f36923add
Laurent Gomila <laurent.gom@gmail.com>
2013-07-01 21:59:46 +0200

    Solved graphics resources not updated or corrupted when loaded in a thread (#411)
    Conflicts:
    	src/sfml/graphics/Font.cpp

--- src/sfml/graphics/Font.cpp
+++ src/sfml/graphics/Font.cpp
@@ -27,6 +27,7 @@
 // Headers
 ////////////////////////////////////////////////////////////////////////////////
 #include <sfml/graphics/Font.hpp>
+#include <sfml/graphics/GLCheck.hpp>
 #include <sfml/system/InputStream.hpp>
 #include <sfml/system/error.hpp>
 #include <ft2build.h>
@@ -480,6 +481,10 @@ Glyph Font::loadGlyph(Uint32 codePoint, unsigned int characterSize, bool bold) c
 
     // Delete the FT glyph
     FT_Done_Glyph(glyphDesc);
+
+    // Force an OpenGL flush, so that the font's texture will appear updated
+    // in all contexts immediately (solves problems in multi-threaded apps)
+    glCheck(glFlush());
 
     // Done :)
     return glyph;
